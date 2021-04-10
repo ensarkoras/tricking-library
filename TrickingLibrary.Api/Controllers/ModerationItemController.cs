7ee6c314dcd7518c39a5cbfeae4da4dcb09bc592
ensarkoras <ensarkoras@gmail.com>
2021-04-10 18:08:52 +0300

    end of ep25

--- TrickingLibrary.Api/Controllers/ModerationItemController.cs
+++ TrickingLibrary.Api/Controllers/ModerationItemController.cs
@@ -1,10 +1,10 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using TrickingLibrary.Api.ViewModels;
 using TrickingLibrary.Data;
 using TrickingLibrary.Models;
 using TrickingLibrary.Models.Moderation;
@@ -26,38 +26,40 @@ namespace TrickingLibrary.Api.Controllers
         public IEnumerable<ModerationItem> All() => _ctx.ModerationItems.ToList();
 
         [HttpGet("{id}")]
-        public ModerationItem Get(int id) =>
-            _ctx.ModerationItems
-                .FirstOrDefault( x=> x.Id.Equals(id));
-        
+        public ModerationItem Get(int id) => _ctx.ModerationItems.FirstOrDefault(x => x.Id.Equals(id));
+
         [HttpGet("{id}/comments")]
-        public IEnumerable<Comment> GetComments(int id) =>
-            _ctx.ModerationItems
-                .Include(x => x.Comments)
-                .Where(x => x.Id.Equals(id))
-                .Select(x => x.Comments)
-                .FirstOrDefault();
-        
+        public IEnumerable<object> GetComments(int id) =>
+            _ctx.Comments
+                .Where(x => x.ModerationItemId.Equals(id))
+                .Select(CommentViewModel.Projection)
+                .ToList();
+
         [HttpPost("{id}/comments")]
         public async Task<IActionResult> Comment(int id, [FromBody] Comment comment)
         {
             var modItem = _ctx.ModerationItems.FirstOrDefault(x => x.Id == id);
 
+            if (modItem == null)
+            {
+                return NoContent();
+            }
+
             var regex = new Regex(@"\B(?<tag>@[a-zA-Z0-9-_]+)");
 
             comment.HtmlContent = regex.Matches(comment.Content)
-                .Aggregate(comment.Content,
-                    (content, match) =>
-                    {
-                        var tag = match.Groups["tag"].Value;
-                        return content
-                            .Replace(tag, $"<a href=\"{tag}-user-link\">{tag}</a>");
-                    });
+                                       .Aggregate(comment.Content,
+                                                  (content, match) =>
+                                                  {
+                                                      var tag = match.Groups["tag"].Value;
+                                                      return content
+                                                         .Replace(tag, $"<a href=\"{tag}-user-link\">{tag}</a>");
+                                                  });
 
             modItem.Comments.Add(comment);
             await _ctx.SaveChangesAsync();
 
-            return Ok(comment);
+            return Ok(CommentViewModel.Create(comment));
         }
     }
 }
